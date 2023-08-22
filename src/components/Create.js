6fe1a930008d8c3e1699fce19f1350d1884e673b
steverino <stvreeno_3@yahoo.com>
2023-08-22 14:30:41 -0400

    uploads

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,42 +1,51 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import ImageUpload from "./ImageUpload";
+
 
 const Create = () => {
   const [title, setTitle] = useState("TITLE");
   const [description, setDescription] = useState("DESCRIPTION");
   const [images, setImages] = useState("IMAGES");
   const [posts, setPosts] = useState([]);
-  
-  useEffect(() => {
-    axios.get("https://api.vschool.io/sfalvo/thing/").then((response) => {
+  const [deleteMsg, setDeleteMsg] = useState("");
+
+  const getImages = () => {
+    axios.get("https://api.vschool.io/sfalvo/thing").then((response) => {
       setPosts(response.data);
     });
+  };
+
+  useEffect(() => {
+    getImages();
   }, []);
-  
 
   const postImage = () => {
     axios
-    .post("https://api.vschool.io/sfalvo/thing", {
-      title: title,
-      description: description,
-      imgUrl: images,
-    })
-    .then((response) => {
-      setPosts((prev)=> [...prev, response.data]);
-      
-    })
-    .catch((error) => console.log(error));
-  };
-  
-  const deletePost = (id) => {
-    axios.delete(`https://api.vschool.io/sfalvo/thing/${id}`)
-    .then((response)=> {posts.filter((post)=> {
-            
-            setPosts((prev)=> [...prev, post.id !== id])
-          
-    })})
+      .post("https://api.vschool.io/sfalvo/thing", {
+        title: title,
+        description: description,
+        imgUrl: images,
+      })
+      .then((response) => {
+        setPosts((prev) => [...prev, response.data]);
+      })
+      .catch((error) => console.log(error));
   };
 
+  const deletePost = (id) => {
+    axios
+      .delete(`https://api.vschool.io/sfalvo/thing/${id}`)
+      .then((response) => {
+        posts.filter((post) => {
+          setDeleteMsg(response.data.msg);
+          setTimeout(() => {
+            setDeleteMsg("");
+          }, 3000);
+          getImages();
+        });
+      });
+  };
 
   return (
     <>
@@ -48,25 +57,31 @@ const Create = () => {
             placeholder="Title"
             onChange={(e) => setTitle(e.target.value)}
           />
+          {/* <input
+            type="text"
+            name="image"
+            placeholder="Image"
+            onChange={(e) => setImages(e.target.value)}
+          /> */}
+          <div>
+          <ImageUpload />
+          </div>
           <input
             type="text"
             name="description"
             placeholder="Description"
             onChange={(e) => setDescription(e.target.value)}
           />
-          <input
-            type="text"
-            name="image"
-            placeholder="Image"
-            onChange={(e) => setImages(e.target.value)}
-          />
         </div>
         <div className="form-button">
           <button className="btn-submit" type="button" onClick={postImage}>
             Submit
           </button>
+          <img src="logo192.png" alt="" />
         </div>
       </form>
+
+      <div id="deleteMessage">{deleteMsg}</div>
       {/* {console.log(Array.isArray(posts))} */}
       <div className="container">
         <ul>
